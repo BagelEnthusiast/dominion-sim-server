0d5b89a7eb8114553b080ee4de49324fdf706e65
Nathan Richardson <nathan.richardson94@gmail.com>
2023-07-12 21:29:34 -0500

    added cors compatibility

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 3000
+const cors = require('cors')
 
 const database =  require("./database.json")
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
