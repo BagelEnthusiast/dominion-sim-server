99282cde63cba489ca6db61fe190853748113431
Nathan Richardson <nathan.richardson94@gmail.com>
2023-07-30 23:04:21 -0500

    now accepts json requests

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import express from "express";
+import express, { response } from "express";
 const app = express();
 const port = 3000
 import cors from 'cors';
 import fs from 'fs';
 
 // todopaul
-
+app.use(express.json())
 app.use(cors())
 
 app.get('/', (req, res) => {
@@ -21,4 +21,11 @@ app.get('/api', (req, res, next) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+app.post('/api/login', (req, res, next) => {
+  console.log('request body', req.body)
+  const { username, password } = req.body;
+  console.log('username, password: ', username, password)
+  return res.json({ message: 'Login Successful', user: username });
 })
